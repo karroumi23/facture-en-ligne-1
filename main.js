0bbc343a078b2899d47900a211dbc292723cbeb3
karroumi <anasskarroumi2016@gmail.com>
2024-05-21 10:24:19 +0200

    create function remove element from company-info

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //******************* start  : remove element from company-info
-
+//   ---create variables
 let spanSiret = document.getElementById("spanSiret");
 let iSiret = document.getElementById("iSiret");
 
@@ -8,5 +8,14 @@ let iTVA = document.getElementById("iTVA");
 
 let spanTél = document.getElementById("spanTél");
 let iTél = document.getElementById("iTél");
-
+// -----create function 
+iSiret.onclick = function(){
+    spanSiret.style.display = 'none';
+}
+iTVA.onclick = function(){
+    spanTVA.style.display = 'none';
+}
+iTél.onclick = function(){
+    spanTél.style.display = 'none';
+}
 //******************* End  : remove element from company-info
