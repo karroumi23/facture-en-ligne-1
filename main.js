48fbcfcdc51ded4f6afa2b9b60bd16945fed8e76
karroumi <anasskarroumi2016@gmail.com>
2024-05-21 10:13:23 +0200

    create variables for function (remove element from company-info)

---
+++ main.js
@@ -0,0 +1,12 @@
+//******************* start  : remove element from company-info
+
+let spanSiret = document.getElementById("spanSiret");
+let iSiret = document.getElementById("iSiret");
+
+let spanTVA = document.getElementById("spanTVA");
+let iTVA = document.getElementById("iTVA");
+
+let spanTél = document.getElementById("spanTél");
+let iTél = document.getElementById("iTél");
+
+//******************* End  : remove element from company-info
