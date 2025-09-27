700a5312ee5968a0f63359240e9dbf33fb493b78
karroumi23 <anasskarroumi2016@gmail.com>
2025-09-27 16:25:50 +0200

    add changes (export'pdf' printing )

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
 // ***************************************** START : create a function to add a logo
-
 let upload = document.getElementById("upload");
 let uploadDiv = document.getElementById("uploadDiv");
 let img = document.getElementById("img");
 
-// upload function
 upload.onchange = function () {
   let file = new FileReader();
   file.readAsDataURL(upload.files[0]);
-
   file.onload = function () {
     img.src = file.result;
     uploadDiv.style.display = "none";
   };
 };
-// ***************************************** END : create a function to add a logo
 
-//************************************* start  : remove element from company-info
-//   ---create variables
+//*******************************************start  company-info
 let spanSiret = document.getElementById("spanSiret");
 let iSiret = document.getElementById("iSiret");
-
 let spanTVA = document.getElementById("spanTVA");
 let iTVA = document.getElementById("iTVA");
-
 let spanTel = document.getElementById("spanTel");
 let iTel = document.getElementById("iTel");
-// -----create function
+
 iSiret.onclick = function () {
   spanSiret.style.display = "none";
 };
@@ -36,114 +29,73 @@ iTVA.onclick = function () {
 iTel.onclick = function () {
   spanTel.style.display = "none";
 };
-//*********************************** End  : remove element from company-info
 
-//******************************** start  : Add an event to the plus icon  company-info
-// Add event listener to the "iAddspan" icon
 document.getElementById("iAddspan").addEventListener("click", function () {
-  // Create a new span element
   var newSpan = document.createElement("span");
   var newInputInfo = document.createElement("input");
-  // Optionally, set attributes for the <input> element
-  newInputInfo.setAttribute("type", "text"); // Set the type of input
+  newInputInfo.setAttribute("type", "text");
   newInputInfo.setAttribute("placeholder", "Information");
   newInputInfo.style.fontWeight = "bold";
 
-  // Create a new input element
   var newInputContenu = document.createElement("input");
   newInputContenu.setAttribute("type", "text");
-  // Set placeholder text for the input
   newInputContenu.setAttribute("placeholder", "Contenu");
 
-  // Create a new icon element
   var newIcon = document.createElement("i");
-  // Assuming you have the appropriate CSS classes
   newIcon.setAttribute("class", "fa-solid fa-circle-minus");
   newIcon.onclick = function () {
     newSpan.style.display = "none";
-  };
-  // Apapend the h4, input, and icon to the new span
+   };
   newSpan.appendChild(newInputInfo);
   newSpan.appendChild(newInputContenu);
   newSpan.appendChild(newIcon);
 
-  // Append the new span to the parent element
   var addSpan = document.getElementById("addSpan");
   addSpan.appendChild(newSpan);
 });
-//*********************************** END  : Add an event to the plus icon  company-info
 
-//******************************************start  : remove element from Client-info
-
+//******************************************start  :  Client-info
 let spanSiretClient = document.getElementById("spanSiretClient");
 let iSiretClient = document.getElementById("iSiretClient");
-
-let spanTVAClient = document.getElementById("spanTVAClient");
-let iTVAClient = document.getElementById("iTVAClient");
-
 let spanTélClient = document.getElementById("spanTélClient");
 let iTélClient = document.getElementById("iTélClient");
 
 iSiretClient.onclick = function () {
   spanSiretClient.style.display = "none";
 };
-iTVA.onclick = function () {
-  spanTVA.style.display = "none";
-};
 iTélClient.onclick = function () {
   spanTélClient.style.display = "none";
 };
-//************************************** */ end  : remove element from Client-info
 
-//******************************** start  : Add an event to the plus icon  Client-info
-
-// Add event listener to the "iAddspan" icon
-document
-  .getElementById("iAddspanClient")
-  .addEventListener("click", function () {
-    // Create a new span element
+document.getElementById("iAddspanClient").addEventListener("click", function () {
     var newSpan = document.createElement("span");
-    // Create a new input element info
     var newInputInfo = document.createElement("input");
-    //  set attributes for the <input> element
-    newInputInfo.setAttribute("type", "text"); // Set the type of input
+    newInputInfo.setAttribute("type", "text");
     newInputInfo.setAttribute("placeholder", "Information");
     newInputInfo.style.fontWeight = "bold";
 
-    // Create a new input element
     var newInputContenu = document.createElement("input");
     newInputContenu.setAttribute("type", "text");
-    // Set placeholder text for the input
     newInputContenu.setAttribute("placeholder", "Contenu");
 
-    // Create a new icon minus
     var newIcon = document.createElement("i");
-    // Assuming you have the appropriate CSS classes
     newIcon.setAttribute("class", "fa-solid fa-circle-minus");
     newIcon.onclick = function () {
       newSpan.style.display = "none";
     };
-    // Apapend the h4, input, and icon to the new span
     newSpan.appendChild(newInputInfo);
     newSpan.appendChild(newInputContenu);
     newSpan.appendChild(newIcon);
 
-    // Append the new span to the parent element
     var addSpan = document.getElementById("addSpanClient");
     addSpan.appendChild(newSpan);
   });
 
-//******************************** END  : Add an event to the plus icon  Client-info
-
 //******************************************************** start : the dates
-// ++++++++++++add ( datelocal ) to the inputs
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the current date and time
   var today = new Date();
-
-  // Format the date to YYYY-MM-DD
   var day = String(today.getDate()).padStart(2, "0");
-  var month = String(today.getMonth() + 1).padStart(2, "0"); // January is 0!
+  var month = String(today.getMonth() + 1).padStart(2, "0");
   var year = today.getFullYear();
   var hours = String(today.getHours()).padStart(2, "0");
   var minutes = String(today.getMinutes()).padStart(2, "0");
@@ -151,7 +103,6 @@ document.addEventListener("DOMContentLoaded", function () {
   var currentDate = year + "-" + month + "-" + day;
   var currentDateTime = currentDate + "T" + hours + ":" + minutes;
 
-  // Set the date input value
   var dateFacture = document.getElementById("dateFacture");
   var dateLivraison = document.getElementById("dateLivraison");
   var datePaiement = document.getElementById("datePaiement");
@@ -161,19 +112,12 @@ document.addEventListener("DOMContentLoaded", function () {
   datePaiement.value = currentDateTime;
 });
 
-/////////////////////////////////////
-//  create variabls
 var trDateFacture = document.getElementById("trDateFacture");
 var iconMinusFacture = document.getElementById("iconMinusFacture");
-
 var trDateLivraison = document.getElementById("trDateLivraison");
 var iconMinusLivraison = document.getElementById("iconMinusLivraison");
-
 var trDatePaiement = document.getElementById("trDatePaiement");
 var iconMinusPaiement = document.getElementById("iconMinusPaiement");
-
-// create function for removing ( tr )
-// from table when i click on icon minus
 
 iconMinusFacture.onclick = function () {
   trDateFacture.style.display = "none";
@@ -184,136 +128,71 @@ iconMinusLivraison.onclick = function () {
 iconMinusPaiement.onclick = function () {
   trDatePaiement.style.display = "none";
 };
-// ++++++++++++++create function add boxes to the table-date
-// Add event listener to the "iAddTrDate" icon
-document.getElementById("iAddTrDate").addEventListener("click", function () {
-  // Create a new (tr) element
-  var newTr = document.createElement("tr");
 
-  // Create new (th) and (input) elements for the left side
+document.getElementById("iAddTrDate").addEventListener("click", function () {
+  var newTr = document.createElement("tr");
   var newThLeft = document.createElement("th");
   var newInputLeft = document.createElement("input");
-
-  // Create new (th) and (input) elements for the right side
   var newThRight = document.createElement("th");
   var newInputRight = document.createElement("input");
 
-  //  set attributes for the(newThLeft & newThRight) elements
   newThLeft.setAttribute("class", "newThLeft");
   newThRight.setAttribute("class", "newThRight");
-
-  //  set attributes for the (input-left) element
   newInputLeft.setAttribute("class", "newInputLeft");
   newInputLeft.setAttribute("placeholder", "En-tête");
-
-  //  set attributes for the (tr) and (input-right) elements
   newTr.setAttribute("class", "newTr");
   newInputRight.setAttribute("class", "newInputRight");
   newInputRight.setAttribute("type", "date");
 
-  // Create a new icon element
   var newIconPlus = document.createElement("i");
-  // Assuming you have the appropriate CSS classes
   newIconPlus.setAttribute("class", "fa-solid fa-circle-minus");
   newIconPlus.onclick = function () {
     newTr.style.display = "none";
   };
 
-  // Append the input to the first th
   newThLeft.appendChild(newInputLeft);
-
-  // Append the input to the second th
   newThRight.appendChild(newInputRight);
-
-  // Append th elements to the tr
   newTr.appendChild(newThLeft);
   newTr.appendChild(newThRight);
   newThRight.appendChild(newIconPlus);
 
-  // Append the new tr to the table or tbody element
   var tableBody = document.getElementById("tableBody");
   tableBody.appendChild(newTr);
 });
 
-//****************************************************** END : the dates
-
 //***************************************************** start : counting-table
-// create function  removing ( tr )
-//  create variabls
 var bottomTr = document.getElementById("bottomTr");
 var iconMinusCount = document.getElementById("iconMinusCoun");
 iconMinusCount.onclick = function () {
   bottomTr.style.display = "none";
 };
 
-//+++++++++ create counting function
-// add variables counting-table
-function getTotal() {
-  let Quantite = document.getElementById("Quantite").value;
-  let unitaire = document.getElementById("unitaire").value;
-  let total = document.getElementById("total");
-  // add variables DEVIS-table
-  let htTtotal = document.getElementById("ht");
-  let tvaTtotal = document.getElementById("tva");
-  let ttcTtotal = document.getElementById("ttc");
-
-  if (Quantite && unitaire) {
-    let result = Quantite * unitaire;
-    total.innerHTML = result.toFixed(2);
-    total.style.background = "#77DD77";
-    htTtotal.value = result.toFixed(2);
-    let pourcentage = result * 0.2;
-    tvaTtotal.value = pourcentage.toFixed(2);
-    ttcTtotal.value = (result + pourcentage).toFixed(2);
-    ttcTtotal.style.background = "#77DD77";
-  } else {
-    total.innerHTML = "";
-  }
-}
-// *
-// *
-// *
-// *
-// +++++++++++++create function add boxes to the counting-table
-// Add event listener to the "iAddTrDate" icon
 document.getElementById("iAddTrCount").addEventListener("click", function () {
-  // Create a new (tr) element
-  //  create variabls
   var newTr = document.createElement("tr");
-
-  // Create a new (th) & inputs
-  //  create variabls
   var thDescription = document.createElement("th");
   var inputDescriptionTop = document.createElement("input");
   var inputDescriptionBottom = document.createElement("input");
-
   var thQuantite = document.createElement("th");
   var inputQuantite = document.createElement("input");
-
   var thUnitaire = document.createElement("th");
   var inputUnitaire = document.createElement("input");
   var spanUnitaire = document.createElement("span");
-
   var thTotal = document.createElement("th");
   var small = document.createElement("small");
   var spanTotal = document.createElement("span");
 
-  //  set attributes for the (TH) and their (input) & (span)
-  // inputDescription
   thDescription.setAttribute("id", "leftThNew");
   inputDescriptionTop.setAttribute("placeholder", "TITRE PRESTATION");
   inputDescriptionTop.setAttribute("type", "text");
   inputDescriptionBottom.setAttribute("placeholder", "Description prestation");
   inputDescriptionBottom.setAttribute("type", "text");
 
-  // inputQuantite
   inputQuantite.setAttribute("value", "1");
   inputQuantite.setAttribute("type", "number");
   inputQuantite.setAttribute("id", "QuantiteNewInput");
   inputQuantite.addEventListener("keyup", updateTotals);
   inputQuantite.addEventListener("change", updateTotals);
 
-  // inputUnitaire
   thUnitaire.setAttribute("id", "thPrixUntair");
   inputUnitaire.setAttribute("placeholder", "00.0");
   inputUnitaire.setAttribute("type", "number");
@@ -321,62 +200,36 @@ document.getElementById("iAddTrCount").addEventListener("click", function () {
   inputUnitaire.addEventListener("keyup", updateTotals);
   inputUnitaire.addEventListener("change", updateTotals);
   spanUnitaire.textContent = "€";
-  // Tota
+
   thTotal.setAttribute("id", "thPrixTotal");
   small.setAttribute("id", "newTtotal");
   small.innerHTML = "0.00";
   spanTotal.textContent = " €";
 
-  // Create a new icon minus
   var newIcon = document.createElement("i");
   newIcon.setAttribute("class", "fa-solid fa-circle-minus");
-  // create func to remove the new (TR)
   newIcon.onclick = function () {
     newTr.remove();
     updateTotals();
-    
   };
 
-  // Append the inputs to their respective th
   thDescription.appendChild(inputDescriptionTop);
   thDescription.appendChild(inputDescriptionBottom);
-
   thQuantite.appendChild(inputQuantite);
-
   thUnitaire.appendChild(inputUnitaire);
   thUnitaire.appendChild(spanUnitaire);
-
   thTotal.appendChild(small);
   thTotal.appendChild(spanTotal);
   thTotal.appendChild(newIcon);
 
-  // Append the th to the tr
   newTr.appendChild(thDescription);
   newTr.appendChild(thQuantite);
   newTr.appendChild(thUnitaire);
   newTr.appendChild(thTotal);
 
-  // Append the new tr to the table element
   var tableCounting = document.getElementById("tableCounting");
   tableCounting.appendChild(newTr);
-
   updateTotals();
-
-
-  // create function to get new total
-//   function getNewTotal() {
-//     // Check if the input quantity is not empty
-//     if (inputQuantite !== "") {
-//       var Nresult = inputQuantite.value * inputUnitaire.value;
-//       small.textContent = Nresult.toFixed(2); // Display the result with two decimal places
-//       small.style.background = "#77DD77";
-        
-//         // Update the total value in the htTtotal input element
-//         let currentHtTotal = parseFloat(htTtotal.value) ; 
-//         htTtotal.value = (currentHtTotal + Nresult).toFixed(2);
-//     }
-// }
-
 });
 
 function updateTotals() {
@@ -411,4 +264,102 @@ function updateTotals() {
   ttcTtotal.value = ttcTotal.toFixed(2);
 }
 
-//************************************************************************** END : counting-table
+// ************ PDF EXPORT FUNCTIONALITY ************
+
+// Function to generate PDF
+async function exportToPDF() {
+  const exportBtn = document.getElementById('exportPdfBtn');
+  const invoiceContent = document.getElementById('invoiceContent');
+  
+  try {
+    // Disable button and show loading
+    exportBtn.disabled = true;
+    exportBtn.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Generating PDF...';
+    
+    // Add PDF mode class to hide edit controls
+    invoiceContent.classList.add('pdf-mode');
+    
+    // Wait a moment for styles to apply
+    await new Promise(resolve => setTimeout(resolve, 100));
+    
+    // Create canvas from HTML
+    const canvas = await html2canvas(invoiceContent, {
+      scale: 2,
+      useCORS: true,
+      allowTaint: true,
+      backgroundColor: '#ffffff',
+      logging: false,
+      width: invoiceContent.offsetWidth,
+      height: invoiceContent.offsetHeight
+    });
+    
+    // Get invoice number for filename
+    const titleElement = document.getElementById('title');
+    const invoiceTitle = titleElement.textContent || 'Facture';
+    const fileName = invoiceTitle.replace(/[^a-zA-Z0-9]/g, '_') + '.pdf';
+    
+    // Create PDF
+    const { jsPDF } = window.jspdf;
+    const imgData = canvas.toDataURL('image/png');
+    
+    // Calculate dimensions
+    const imgWidth = 210; // A4 width in mm
+    const pageHeight = 295; // A4 height in mm
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    let heightLeft = imgHeight;
+    
+    const pdf = new jsPDF('p', 'mm', 'a4');
+    let position = 0;
+    
+    // Add first page
+    pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+    heightLeft -= pageHeight;
+    
+    // Add additional pages if needed
+    while (heightLeft >= 0) {
+      position = heightLeft - imgHeight;
+      pdf.addPage();
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+    }
+    
+    // Save the PDF
+    pdf.save(fileName);
+    
+  } catch (error) {
+    console.error('Error generating PDF:', error);
+    alert('Une erreur est survenue lors de la génération du PDF. Veuillez réessayer.');
+  } finally {
+    // Remove PDF mode class
+    invoiceContent.classList.remove('pdf-mode');
+    
+    // Re-enable button
+    exportBtn.disabled = false;
+    exportBtn.innerHTML = '<i class="fas fa-file-pdf"></i> Export to PDF';
+  }
+}
+
+// Function to print invoice
+function printInvoice() {
+  const invoiceContent = document.getElementById('invoiceContent');
+  
+  // Add PDF mode class to hide edit controls
+  invoiceContent.classList.add('pdf-mode');
+  
+  // Print
+  window.print();
+  
+  // Remove PDF mode class after printing
+  setTimeout(() => {
+    invoiceContent.classList.remove('pdf-mode');
+  }, 1000);
+}
+
+// Event listeners for export buttons
+document.getElementById('exportPdfBtn').addEventListener('click', exportToPDF);
+document.getElementById('printBtn').addEventListener('click', printInvoice);
+
+// Initialize totals on page load
+document.addEventListener('DOMContentLoaded', function() {
+  updateTotals();
+});
